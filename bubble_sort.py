ae9e8c9361a61f89adfecfa68f1316a81f8f08ff
Farouk-Ezz-ElArab <faroukezz9595@gmail.com>
2026-02-25 17:07:27 +0200

    codes

---
+++ bubble_sort.py
@@ -0,0 +1,15 @@
+def bubble_sort(arr):
+    sorted = False
+    n = len(arr)
+    for i in range (n):
+        sorted = True
+        for j in range (0,n-i-1):
+            if arr[j] > arr[j+1]:
+                sorted = False
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+        if sorted:
+            break
+    return arr
+w
+arr = [64, 34, 25, 1, 22, 11, 90]
+print("Sorted array is:", bubble_sort(arr))
